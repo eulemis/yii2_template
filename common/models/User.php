a163cac2515a08c53f808a9de453aeb699c744d6
eulemis <eulemisalfredo@gmail.com>
2017-03-06 01:16:21 -0430

    Signed-off-by: eulemis <eulemisalfredo@gmail.com>

--- common/models/User.php
+++ common/models/User.php
@@ -6,7 +6,15 @@ use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
-
+use yii\db\Expression;
+use yii\helpers\Security;
+use backend\models\Role;
+use yii\helpers\ArrayHelper;
+use backend\models\Status;
+use backend\models\UserType;
+use frontend\models\Profile;
+use yii\helpers\Url;
+use yii\helpers\Html;
 /**
  * User model
  *
@@ -23,7 +31,7 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const STATUS_DELETED = 0;
+    //const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
 
 
@@ -41,8 +49,15 @@ class User extends ActiveRecord implements IdentityInterface
     public function behaviors()
     {
         return [
-            TimestampBehavior::className(),
-        ];
+            'timestamp' => [
+            'class' => 'yii\behaviors\TimestampBehavior',
+            'attributes' => [
+                ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                            ],
+            'value' => new Expression('NOW()'),
+                            ],
+            ];
     }
 
     /**
@@ -51,139 +66,241 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
-        ];
+            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
+            [['status_id'],'in', 'range'=>array_keys($this->getStatusList())],
+            ['role_id', 'default', 'value' => 10],
+            [['role_id'],'in', 'range'=>array_keys($this->getRoleList())],
+            ['user_type_id', 'default', 'value' => 10],
+            [['user_type_id'],'in', 'range'=>array_keys($this->getUserTypeList())],
+            ['username', 'filter', 'filter' => 'trim'],
+            ['username', 'required'],
+            ['username', 'unique'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'unique'],
+         ];
     }
+
+    public function attributeLabels()
+    {
+        return [
+        /* Your other attribute labels */
+        'roleName' => Yii::t('app', 'Role'),
+        'statusName' => Yii::t('app', 'Status'),
+        'profileId' => Yii::t('app', 'Profile'),
+        'profileLink' => Yii::t('app', 'Profile'),
+        'userLink' => Yii::t('app', 'User'),
+        'username' => Yii::t('app', 'User'),
+        'userTypeName' => Yii::t('app', 'User Type'),
+        'userTypeId' => Yii::t('app', 'User Type'),
+        'userIdLink' => Yii::t('app', 'ID'),
+        ];
+}
 
     /**
      * @inheritdoc
      */
     public static function findIdentity($id)
     {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+     return static::findOne(['id' => $id, 'status_id' => self::STATUS_ACTIVE]);
     }
-
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
-    }
-
-    /**
-     * Finds user by password reset token
-     *
-     * @param string $token password reset token
-     * @return static|null
-     */
-    public static function findByPasswordResetToken($token)
-    {
-        if (!static::isPasswordResetTokenValid($token)) {
-            return null;
-        }
-
-        return static::findOne([
-            'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
-        ]);
-    }
-
-    /**
-     * Finds out if password reset token is valid
-     *
-     * @param string $token password reset token
-     * @return bool
-     */
-    public static function isPasswordResetTokenValid($token)
-    {
-        if (empty($token)) {
-            return false;
-        }
-
-        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
-        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
-        return $timestamp + $expire >= time();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getId()
-    {
-        return $this->getPrimaryKey();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAuthKey()
-    {
-        return $this->auth_key;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->getAuthKey() === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return Yii::$app->security->validatePassword($password, $this->password_hash);
-    }
-
-    /**
-     * Generates password hash from password and sets it to the model
-     *
-     * @param string $password
-     */
-    public function setPassword($password)
-    {
-        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
-    }
-
-    /**
-     * Generates "remember me" authentication key
-     */
-    public function generateAuthKey()
-    {
-        $this->auth_key = Yii::$app->security->generateRandomString();
-    }
-
-    /**
-     * Generates new password reset token
-     */
-    public function generatePasswordResetToken()
-    {
-        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
-    }
-
-    /**
-     * Removes password reset token
-     */
+/**
+* @findIdentityByAccessToken
+*/
+public static function findIdentityByAccessToken($token, $type = null)
+{
+    return static::findOne(['auth_key' => $token]);
+}
+/**
+* Finds user by username
+* broken into 2 lines to avoid wordwrapping * @param string $username
+* @return static|null
+*/
+public static function findByUsername($username)
+{
+    return static::findOne(['username' => $username, 'status_id' =>
+    self::STATUS_ACTIVE]);
+}
+/**
+* Finds user by password reset token
+*
+* @param string $token password reset token
+* @return static|null
+*/
+public static function findByPasswordResetToken($token)
+{
+    $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+    $parts = explode('_', $token);
+    $timestamp = (int) end($parts);
+    if ($timestamp + $expire < time()) {
+        // token expired
+    return null;
+}
+    return static::findOne([
+    'password_reset_token' => $token,
+    'status_id' => self::STATUS_ACTIVE,
+]);
+}
+/**
+* @getId
+*/
+public function getId()
+{
+    return $this->getPrimaryKey();
+}
+/**
+* @getAuthKey
+*/
+public function getAuthKey()
+{
+    return $this->auth_key;
+}
+/**
+* @validateAuthKey
+*/
+public function validateAuthKey($authKey)
+{
+    return $this->getAuthKey() === $authKey;
+}
+/**
+* Validates password
+*
+* @param string $password password to validate
+* @return boolean if password provided is valid for current user
+*/
+public function validatePassword($password)
+{
+    return Yii::$app->security->validatePassword($password, $this->password_hash);
+}
+/**
+* Generates password hash from password and sets it to the model
+*
+* @param string $password
+*/
+/**
+* Generates password hash from password and sets it to the model
+*
+* @param string $password
+*/
+public function setPassword($password)
+{
+    $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+}
+/**
+* Generates "remember me" authentication key
+*/
+public function generateAuthKey()
+{
+    $this->auth_key = Yii::$app->security->generateRandomString();
+}
+/**
+* Generates new password reset token
+* broken into 2 lines to avoid wordwrapping
+*/
+public function generatePasswordResetToken()
+{
+$this->password_reset_token = Yii::$app->security->generateRandomString()
+    . '_' . time();
+}
+/**
+* Removes password reset token
+*/
     public function removePasswordResetToken()
     {
         $this->password_reset_token = null;
+    }
+
+
+    public function getRole()
+    {
+        return $this->hasOne(Role::className(), ['role_value' => 'role_id']);
+    }
+
+    public function getRoleName()
+    {
+        return $this->role ? $this->role->role_name : '- no role -';
+    }
+
+    public static function getRoleList()
+    {
+        $droptions = Role::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'role_value', 'role_name');
+    }
+
+    public function getStatus()
+    {
+        return $this->hasOne(Status::className(), ['status_value' => 'status_id']);
+    }
+    public function getStatusName()
+    {
+        return $this->status ? $this->status->status_name : '- no status -';
+    }
+    /**
+    * get list of statuses for dropdown
+    */
+    public static function getStatusList()
+    {
+        $droptions = Status::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'status_value', 'status_name');
+    }
+
+    public function getUserType()
+    {
+        return $this->hasOne(UserType::className(), ['user_type_value' =>
+        'user_type_id']);
+    }
+    /**
+    * get user type name
+    *
+    */
+    public function getUserTypeName()
+    {
+        return $this->userType ? $this->userType->user_type_name : '- no user type -';
+    }
+    /**
+    * get list of user types for dropdown
+    */
+    public static function getUserTypeList()
+    {
+        $droptions = UserType::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'user_type_value', 'user_type_name');
+    }
+
+    public function getUserTypeId()
+    {
+    return $this->userType ? $this->userType->id : 'none';
+    }
+
+    public function getProfile()
+    {
+        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+
+    public function getProfileId()
+    {
+    return $this->profile ? $this->profile->id : 'none';
+    }
+
+    public function getProfileLink()
+    {
+        $url = Url::to(['profile/view', 'id'=>$this->profileId]);
+        $options = [];
+        return Html::a($this->profile ? 'profile' : 'none', $url, $options);
+    }
+
+    public function getUserIdLink()
+    {
+        $url = Url::to(['user/update', 'id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id, $url, $options);
+    }
+
+    public function getUserLink()
+    {
+        $url = Url::to(['user/view', 'id'=>$this->Id]);
+        $options = [];
+        return Html::a($this->username, $url, $options);
     }
 }
